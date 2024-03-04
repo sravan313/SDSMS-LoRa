f76a4d3a8b2a7d9990e995ffa1229d4abf4b070f
sravan313 <118177576+sravan313@users.noreply.github.com>
2024-03-04 17:42:37 +0530

    Add files via upload

---
+++ lora_packages.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get upgrade --fix-missing
+pip install RPi.GPIO
+pip install spidev
+pip install pyLoRa
+sudo apt-get install python-rpi.gpio
+sudo apt-get install python3-rpi.gpio
+sudo apt-get install python-spidev
+sudo apt-get install python3-spidev
+sudo apt-get install git
+pip3 install --upgrade pycrypto
+pip install rsa
+sudo apt-get install bluetooth libbluetooth-dev
+sudo python3 -m pip install pybluez
+sudo apt install bluetooth pi-bluetooth bluez blueman
+python3 -m pip install fpdf
+sudo apt-get update
+sudo apt-get upgrade --fix-missing
+sudo apt-get update
